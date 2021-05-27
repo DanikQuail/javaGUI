8e436cec2a3822532ec48cdaa780b7a25d3d5e43
d1saabl3 <73097704+d1saabl3@users.noreply.github.com>
2021-05-27 08:41:16 +0200

    ano

---
+++ src/main/java/cz/educanet/gui/Controller.java
@@ -0,0 +1,14 @@
+package cz.educanet.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class Controller {
+    @FXML
+
+
+    public void onClickVypocitat(ActionEvent actionEvent) {
+    }
+}

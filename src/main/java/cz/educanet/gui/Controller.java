72f844704382a187725044c1c3ebf460316f16c1
d1saabl3 <73097704+d1saabl3@users.noreply.github.com>
2021-06-10 15:58:16 +0200

    Adventure text game

--- src/main/java/cz/educanet/gui/Controller.java
+++ src/main/java/cz/educanet/gui/Controller.java
@@ -2,13 +2,21 @@ package cz.educanet.gui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+import javafx.scene.control.Button;
 
 import java.awt.*;
 
+
 public class Controller {
     @FXML
+    public Button startButton;
+    public Button exitButton;
 
+    public void onClickedStart(ActionEvent actionEvent) {
 
-    public void onClickVypocitat(ActionEvent actionEvent) {
+    }
+    public void onClickedExit(ActionEvent actionEvent) {
+        System.exit(0);
     }
 }
--- src/test/java/cz/educanet/Main.java
+++
@@ -1,9 +0,0 @@
-package cz.educanet;
-
-import cz.educanet.gui.Window;
-
-public class Main {
-    public static void main(String[] args) {
-        Window.main(args);
-    }
-}

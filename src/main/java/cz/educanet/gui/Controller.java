e47a5ce2bc6397a598e4a2c7836e2c31b848d802
d1saabl3 <73097704+d1saabl3@users.noreply.github.com>
2021-06-10 22:55:15 +0200

    VELMI EPICKA HRA S EPICKYM KONCEM !!!!!!!!!!!!!!!!!!!!!

--- src/main/java/cz/educanet/gui/Controller.java
+++ src/main/java/cz/educanet/gui/Controller.java
@@ -12,17 +12,24 @@ public class Controller {
     @FXML
     public Button firstButton;
     public Button secondButton;
+    public Button wowButton;
     public Button exitButton;
     public Text textDesc;
 
     public void onClickedFirst(ActionEvent actionEvent) {
-        textDesc.setText("Sel si doleva a nasel si velmi starou truhlu, otevrel si ji a vevnitr byl lektvar, vypijes ho ?");
-
-    }
-    public void onClickedSecond(ActionEvent actionEvent) {
-        textDesc.setText("Sel si doprava a v pulce cesty te bodl typek nozem do zad, umrel si");
+        textDesc.setText("Umrel si");
         firstButton.setVisible(false);
         secondButton.setVisible(false);
+    }
+    public void onClickedSecond(ActionEvent actionEvent) {
+        textDesc.setText("Sel si doleva a nasel si velmi starou truhlu, otevrel si ji a vevnitr byl lektvar, vypijes ho ?");
+        firstButton.setText("Ano");
+        secondButton.setVisible(false);
+    }
+    public void onClickedNevimTohleJeTakovejTrash(ActionEvent actionEvent){
+        textDesc.setText("Radsi sis rekl ze pudes domu protoze je tma. KONEC ");//ano velmi epicka hra, fakt jsem nevedel jak neco udelat a kopirovat kod jsem uz nechtel vubec delat
+        firstButton.setVisible(false);
+        wowButton.setVisible(false);
     }
     public void onClickedExit(ActionEvent actionEvent) {
         System.exit(0);

14322d4a213b480557fddf6feadbd7684a3d1bdf
d1saabl3 <73097704+d1saabl3@users.noreply.github.com>
2021-06-10 22:39:02 +0200

    Trash

--- src/main/java/cz/educanet/gui/Controller.java
+++ src/main/java/cz/educanet/gui/Controller.java
@@ -10,11 +10,19 @@ import java.awt.*;
 
 public class Controller {
     @FXML
-    public Button startButton;
+    public Button firstButton;
+    public Button secondButton;
     public Button exitButton;
+    public Text textDesc;
 
-    public void onClickedStart(ActionEvent actionEvent) {
+    public void onClickedFirst(ActionEvent actionEvent) {
+        textDesc.setText("Sel si doleva a nasel si velmi starou truhlu, otevrel si ji a vevnitr byl lektvar, vypijes ho ?");
 
+    }
+    public void onClickedSecond(ActionEvent actionEvent) {
+        textDesc.setText("Sel si doprava a v pulce cesty te bodl typek nozem do zad, umrel si");
+        firstButton.setVisible(false);
+        secondButton.setVisible(false);
     }
     public void onClickedExit(ActionEvent actionEvent) {
         System.exit(0);
